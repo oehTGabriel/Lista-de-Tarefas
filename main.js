256caa7a9867c6b9bbe37d98b1e1c93602766d69
Theo Gabriel <theonbgabriel@gmail.com>
2023-10-30 10:48:23 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e){
+        e.preventDefault();
+
+        const novaTarefa = $('#tarefa').val();
+        const novoItem = $(`<li> </li>`);
+
+        $(`<h2>${novaTarefa}</h2>`).appendTo(novoItem);
+
+        $(novoItem).appendTo('ul');
+    })
+})
